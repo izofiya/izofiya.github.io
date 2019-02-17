cceb4f6196535d4c4fd3921383aa70e1cae91a59
Izofiya <46092521+izofiya@users.noreply.github.com>
2019-02-17 21:27:19 +0300

    Added a react-app

---
+++ build/precache-manifest.29ec126f215d79db822e40e634f0f481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6701b85c780ad41814b",
+    "url": "/static/css/main.04144122.chunk.css"
+  },
+  {
+    "revision": "d6701b85c780ad41814b",
+    "url": "/static/js/main.d6701b85.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7adff4f01c99f40f0dd026d315d7629f",
+    "url": "/index.html"
+  }
+];
